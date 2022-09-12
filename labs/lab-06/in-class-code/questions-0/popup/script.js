cb464e9a862c64265de200b846e8c7ebff629c73
Leon Eckert <leoneckert@mailbox.org>
2022-09-12 21:43:33 +0800

    took out labs21

--- labs/lab-06/in-class-code/questions-0/popup/script.js
+++
@@ -1,9 +0,0 @@
-
-chrome.runtime.sendMessage({type: "getQuestions"}, function(response) {
-  console.log(response);
-  for(let i = 0; i < response.length; i++){
-    let li = document.createElement("li");
-    li.innerHTML = response[i];
-    document.getElementById("list").appendChild(li)
-  }
-});
