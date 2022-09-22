c53add56aa50ef3e8db38913b0b4b821d8e696d2
Leon Eckert <leoneckert@mailbox.org>
2022-09-22 17:53:31 +0800

    acc

--- labs/mobileAPI/gyroscope/script.js
+++ labs/mobileAPI/gyroscope/script.js
@@ -44,6 +44,13 @@ function permission () {
                     document.getElementById("beta").innerHTML = event.beta;
                     document.getElementById("gamma").innerHTML = event.gamma;
                 });
+                window.addEventListener('devicemotion', (event) => {
+                    // console.log(`${event.acceleration.x} m/s2`);
+                    document.getElementById("acc_x").innerHTML = event.acceleration.x;
+                    document.getElementById("acc_y").innerHTML = event.acceleration.y;
+
+
+                });
             }
         })
             .catch( console.error )
